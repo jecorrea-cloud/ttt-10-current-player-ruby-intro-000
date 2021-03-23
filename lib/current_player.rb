37e67f1016dfb9f31c74500656eafe05a0c7086e
Joaquin Correa <jecorrea@protonmail.com>
2021-03-23 22:01:49 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,15 @@
+def turn_count(board)
+  counter = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      counter +=1
+    end
+  end
+  counter
+end
+
+
 def current_player
-
 end
 
-def turn_count
-end
+
